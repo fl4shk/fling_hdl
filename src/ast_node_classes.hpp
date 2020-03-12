6ee03bcaf0086d7542cfcd668efbbc72580aa99f
fl4shk <fl4shk@users.noreply.github.com>
2020-03-12 18:44:15 -0500

    working towards AST nodes

--- src/ast_node_classes.hpp
+++ src/ast_node_classes.hpp
@@ -13,6 +13,7 @@ class AstVisitor;
 
 namespace ast
 {
+
 #define SHARED_FUNC_CONTENTS(name) \
 	inline name() = default; \
 	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(name); \
@@ -71,7 +72,7 @@ public:		// functions
 };
 
 class DeclParamList_Item;
-class DeclParamList: public HasChildrenBase<DeclParamList_Item*>
+class DeclParamList: public HasChildrenBase<shared_ptr<DeclParamList_Item>>
 {
 public:		// functions
 	SHARED_FUNC_CONTENTS(DeclParamList);
@@ -105,7 +106,7 @@ public:		// functions
 };
 
 class DeclArgList_Item;
-class DeclArgList: public HasChildrenBase<DeclArgList_Item*>
+class DeclArgList: public HasChildrenBase<shared_ptr<DeclArgList_Item>>
 {
 public:		// functions
 	SHARED_FUNC_CONTENTS(DeclArgList);
@@ -151,8 +152,8 @@ public:		// functions
 
 class Expr;
 class TypenameOrModname;
-class InstParamList_Pos: public HasChildrenBase<variant<Expr*,
-	TypenameOrModname*>>
+class InstParamList_Pos: public HasChildrenBase<variant<shared_ptr<Expr>,
+	shared_ptr<TypenameOrModname>>>
 {
 public:		// functions
 	SHARED_FUNC_CONTENTS(InstParamList_Pos);
@@ -160,25 +161,24 @@ public:		// functions
 
 class InstParamList_Named_Item;
 class InstParamList_Named:
-	public HasChildrenBase<InstParamList_Named_Item*>
+	public HasChildrenBase<shared_ptr<InstParamList_Named_Item>>
 {
 public:		// functions
 	SHARED_FUNC_CONTENTS(InstParamList_Named);
 };
 
-class Ident;
 class InstParamList_Named_Item: public Base
 {
 public:		// types
 	using Rhs = typename InstParamList_Pos::Child;
 
 private:		// variables
-	shared_ptr<Ident> _name = nullptr;
+	string _name;
 	Rhs _rhs;
 public:		// functions
 	SHARED_FUNC_CONTENTS(InstParamList_Named_Item);
 
-	GEN_GETTER_AND_SETTER_BY_VAL(name);
+	GEN_GETTER_BCR_AND_SETTER_BRR(name);
 	GEN_GETTER_BCR_AND_SETTER_BRR(rhs);
 };
 
@@ -200,6 +200,84 @@ class InstArgList_Named:
 {
 public:		// functions
 	SHARED_FUNC_CONTENTS(InstArgList_Named);
+};
+
+class InstArgList_Named_Item: public Base
+{
+private:		// variables
+	string _name;
+	shared_ptr<Expr> _expr;
+public:		// functions
+	SHARED_FUNC_CONTENTS(InstArgList_Named_Item);
+
+	GEN_GETTER_BCR_AND_SETTER_BRR(name);
+	GEN_GETTER_BCR_AND_SETTER_BRR(expr);
+};
+
+class ImportList;
+class DeclModule_Scope;
+class DeclModule: public Base
+{
+public:		// types
+	using OptDeclParamList = optional<shared_ptr<DeclParamList>>;
+	using OptImportList = optional<shared_ptr<ImportList>>;
+private:		// variables
+	string _name;
+	OptDeclParamList _opt_decl_param_list;
+	shared_ptr<DeclArgList> _decl_arg_list;
+	OptImportList _opt_import_list;
+	shared_ptr<DeclModule_Scope> _decl_module_scope;
+public:		// functions
+	SHARED_FUNC_CONTENTS(DeclModule);
+
+	GEN_GETTER_BCR_AND_SETTER_BRR(name);
+	GEN_GETTER_BCR_AND_SETTER_BRR(opt_decl_param_list);
+	GEN_GETTER_BCR_AND_SETTER_BRR(decl_arg_list);
+	GEN_GETTER_BCR_AND_SETTER_BRR(opt_import_list);
+	GEN_GETTER_BCR_AND_SETTER_BRR(decl_module_scope);
+};
+
+class InstModule;
+class Expr_CallSubprog;
+class Generate;
+class ContAssign;
+class Behav;
+class DeclWire;
+class DeclVar;
+
+class DeclModule_Scope: public HasChildrenBase<variant
+	<shared_ptr<InstModule>, shared_ptr<Expr_CallSubprog>,
+	shared_ptr<Generate>, shared_ptr<ContAssign>, shared_ptr<ImportList>,
+	shared_ptr<Behav>, shared_ptr<DeclWire>, shared_ptr<DeclVar>,
+	shared_ptr<DeclConst>, shared_ptr<DeclType>, shared_ptr<DeclSubprog>,
+	shared_ptr<DeclAlias>>>
+{
+public:		// functions
+	SHARED_FUNC_CONTENTS(DeclModule_Scope);
+};
+
+class InstModule: public Base
+{
+private:		// variables
+	string _name;
+	shared_ptr<TypenameOrModname> _typename_or_modname;
+	shared_ptr<InstArgList> _inst_arg_list;
+public:		// functions
+	SHARED_FUNC_CONTENTS(InstModule);
+
+	GEN_GETTER_BCR_AND_SETTER_BRR(name);
+	GEN_GETTER_BCR_AND_SETTER_BRR(typename_or_modname);
+	GEN_GETTER_BCR_AND_SETTER_BRR(inst_arg_list);
+};
+
+class Generate_If;
+class Generate_Switch;
+class Generate_For;
+class Generate: public Base
+{
+public:		// types
+	using SpecificGenerate = variant<shared_ptr<Generate_If>,
+		shared_ptr<Generate_Switch>, shared_ptr<Generate_For>>;
 };
 
 #undef SHARED_FUNC_CONTENTS
