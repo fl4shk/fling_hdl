908e090ca8dd8181ed920f587303fe38ad3e6bea
fl4shk <fl4shk@users.noreply.github.com>
2020-03-29 14:16:46 -0500

    more AST printing stuff

--- src/ast_node_classes.cpp
+++ src/ast_node_classes.cpp
@@ -35,12 +35,13 @@ using namespace fling_hdl::ast;
 
 #define idcmp(...) if (_inner_idcmp<__VA_ARGS__>(node))
 
-#define wrap(a, b) \
-	#a, "(", b, ")"
-
 // "Node as"
-#define ndas(type) \
-	static_cast<type*>(node)
+#define ndas \
+	static_cast<TYPE*>(node)
+
+#define wrap(item) \
+	sconcat(#item, "(", ndas->item, ")")
+
 
 std::ostream& operator << (std::ostream& os, const BaseSptrList& list)
 {
@@ -53,21 +54,75 @@ std::ostream& operator << (std::ostream& os, const BaseSptrList& list)
 
 std::ostream& operator << (std::ostream& os, Base* node)
 {
-	osprintout(os, node->id(), "(", wrap(fp, node->fp()), "\n");
-	idcmp(Program)
+	if (node == nullptr)
 	{
-		osprintout(os, wrap("item_list", ndas(Program)->item_list));
-	}
-	else idcmp(DeclPackage)
-	{
-	}
-	else idcmp(DeclParamList)
-	{
-	}
-	else idcmp(DeclParamList_Item)
-	{
+		osprintout(os, "nullptr");
 	}
 
-	osprintout(os, ")");
+	else // if (node != nullptr)
+	{
+		osprintout(os, node->id(), "(", "fp", "(", node->fp(), ")", "\n");
+		idcmp(Program)
+		{
+			#undef TYPE
+			#define TYPE Program
+			osprintout(os, wrap(item_list));
+		}
+		else idcmp(DeclPackage)
+		{
+			#undef TYPE
+			#define TYPE DeclPackage
+			osprintout(os, strappcom2(wrap(ident), wrap(item_list)));
+		}
+		else idcmp(DeclParamList)
+		{
+			#undef TYPE
+			#define TYPE DeclParamList
+			osprintout(os, strappcom2(wrap(item_list)));
+		}
+		else idcmp(DeclParamList_Item)
+		{
+			#undef TYPE
+			#define TYPE DeclParamList_Item
+			osprintout(os, strappcom2(wrap(ident_list)));
+
+			if (std::holds_alternative<DeclParamList_Item::LocalVar>
+				(ndas->post_ident_list))
+			{
+				auto& local_var = std::get<DeclParamList_Item::LocalVar>
+					(ndas->post_ident_list);
+
+				osprintout(os, strappcom2("kind(local_var)",
+					sconcat("typename_or_modname(", 
+						local_var.typename_or_modname, ")"),
+					sconcat("opt_expr_list(",
+						local_var.opt_expr_list, ")")));
+			}
+			else if (std::holds_alternative<DeclParamList_Item
+				::LocalTypename>(ndas->post_ident_list))
+			{
+				auto& local_typename = std::get<DeclParamList_Item
+					::LocalTypename>(ndas->post_ident_list);
+
+				osprintout(os, strappcom2("kind(typename)",
+					sconcat("opt_typename_or_modname_list(",
+						local_typename.opt_typename_or_modname_list,
+						")")));
+			}
+			else if (std::holds_alternative<DeclParamList_Item
+				::LocalModname>(ndas->post_ident_list))
+			{
+				auto& local_modname = std::get<DeclParamList_Item
+					::LocalModname>(ndas->post_ident_list);
+
+				osprintout(os, strappcom2("kind(modname)",
+					sconcat("opt_typename_or_modname_list(",
+						local_modname.opt_typename_or_modname_list,
+						")")));
+			}
+		}
+
+		osprintout(os, ")");
+	}
 	return os;
 }
