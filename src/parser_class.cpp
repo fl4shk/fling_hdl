d068f3a77a0925fca33f0ff2ee4037e314011fa1
fl4shk <fl4shk@users.noreply.github.com>
2020-05-13 08:28:09 -0500

    stuffs

--- src/parser_class.cpp
+++ src/parser_class.cpp
@@ -1,1 +1,365 @@
 #include "parser_class.hpp"
+
+namespace fling_hdl
+{
+#define perf_rgr(func) \
+	_recrs_get_rules(this, &Parser::func, &rgr_ret)
+
+void Parser::parseFlingProgram()
+{
+	while (_lexer->tok() != Tok::MiscEof)
+	{
+		RecrsGetRulesRet rgr_ret;
+		perf_rgr(_parseFlingDeclPackageItem);
+	}
+}
+void Parser::_parseFlingDeclPackage()
+{
+}
+void Parser::_parseFlingDeclPackageItem()
+{
+}
+void Parser::_parseFlingImport()
+{
+}
+void Parser::_parseFlingDeclParamList()
+{
+}
+void Parser::_parseFlingDeclParamListItem()
+{
+}
+void Parser::_parseFlingDeclArgList()
+{
+}
+void Parser::_parseFlingDeclArgListItem()
+{
+}
+void Parser::_parseFlingInstParamList()
+{
+}
+void Parser::_parseFlingInstParamListPos()
+{
+}
+void Parser::_parseFlingInstParamListPosItem()
+{
+}
+void Parser::_parseFlingInstParamListNamed()
+{
+}
+void Parser::_parseFlingInstParamListNamedItem()
+{
+}
+void Parser::_parseFlingInstArgList()
+{
+}
+void Parser::_parseFlingInstArgListPos()
+{
+}
+void Parser::_parseFlingInstArgListPosItem()
+{
+}
+void Parser::_parseFlingInstArgListNamed()
+{
+}
+void Parser::_parseFlingInstArgListNamedItem()
+{
+}
+void Parser::_parseFlingDeclModule()
+{
+}
+void Parser::_parseFlingDeclModuleScope()
+{
+}
+void Parser::_parseFlingDeclModuleItem()
+{
+}
+void Parser::_parseFlingModinst()
+{
+}
+void Parser::_parseFlingDeclModuleGen()
+{
+}
+void Parser::_parseFlingDeclModuleGenIf()
+{
+}
+void Parser::_parseFlingDeclModuleGenSwitchEtc()
+{
+}
+void Parser::_parseFlingDeclModuleGenSwitchEtcCase()
+{
+}
+void Parser::_parseFlingDeclModuleGenSwitchEtcDefault()
+{
+}
+void Parser::_parseFlingDeclModuleGenFor()
+{
+}
+void Parser::_parseFlingDeclModuleBehav()
+{
+}
+void Parser::_parseFlingDeclModuleBehavComb()
+{
+}
+void Parser::_parseFlingDeclModuleBehavSeq()
+{
+}
+void Parser::_parseFlingDeclModuleBehavSeqEdgeItem()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScope()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItem()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemIf()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemSwitchEtc()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemSwitchEtcCase()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemSwitchEtcDefault()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemFor()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemWhile()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemGen()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemGenIf()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemGenSwitchEtc()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemGenSwitchEtcCase()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemGenSwitchEtcDefault()
+{
+}
+void Parser::_parseFlingDeclModuleBehavScopeItemGenFor()
+{
+}
+void Parser::_parseFlingAnyBehavScopeItem()
+{
+}
+void Parser::_parseFlingAnyBehavScopeItemAssign()
+{
+}
+void Parser::_parseFlingDeclCompositeType()
+{
+}
+void Parser::_parseFlingDeclCompositeTypeStruct()
+{
+}
+void Parser::_parseFlingDeclCompositeTypeStructItem()
+{
+}
+void Parser::_parseFlingCompositeTypeStructItemGen()
+{
+}
+void Parser::_parseFlingCompositeTypeStructItemGenIf()
+{
+}
+void Parser::_parseFlingCompositeTypeStructItemGenSwitchEtc()
+{
+}
+void Parser::_parseFlingCompositeTypeStructItemGenSwitchEtcCase()
+{
+}
+void Parser::_parseFlingCompositeTypeStructItemGenSwitchEtcDefault()
+{
+}
+void Parser::_parseFlingCompositeTypeStructItemGenFor()
+{
+}
+void Parser::_parseFlingDeclEnum()
+{
+}
+void Parser::_parseFlingDeclEnumItem()
+{
+}
+void Parser::_parseFlingDeclSubprog()
+{
+}
+void Parser::_parseFlingDeclSubprogFuncHeader()
+{
+}
+void Parser::_parseFlingDeclSubprogTaskHeader()
+{
+}
+void Parser::_parseFlingDeclSubprogScope()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItem()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemIf()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemSwitchEtc()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemSwitchEtcCase()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemSwitchEtcDefault()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemFor()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemWhile()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemGen()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemGenIf()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemGenSwitchEtc()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemGenSwitchEtcCase()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemGenSwitchEtcDefault()
+{
+}
+void Parser::_parseFlingDeclSubprogScopeItemGenFor()
+{
+}
+void Parser::_parseFlingDeclConst()
+{
+}
+void Parser::_parseFlingDeclVarNoInitVal()
+{
+}
+void Parser::_parseFlingDeclVar()
+{
+}
+void Parser::_parseFlingDeclWire()
+{
+}
+void Parser::_parseFlingWireAssign()
+{
+}
+void Parser::_parseFlingDeclAlias()
+{
+}
+void Parser::_parseFlingIdent()
+{
+}
+void Parser::_parseFlingScopedIdent()
+{
+}
+void Parser::_parseFlingIdentList()
+{
+}
+void Parser::_parseFlingSubprogIdentList()
+{
+}
+void Parser::_parseFlingExprList()
+{
+}
+void Parser::_parseFlingRangeList()
+{
+}
+void Parser::_parseFlingExprOrRangeList()
+{
+}
+void Parser::_parseFlingTypenmOrModnmList()
+{
+}
+void Parser::_parseFlingImportItem()
+{
+}
+void Parser::_parseFlingImportItemList()
+{
+}
+void Parser::_parseFlingExpr()
+{
+}
+void Parser::_parseFlingMuxExpr()
+{
+}
+void Parser::_parseFlingLogorExpr()
+{
+}
+void Parser::_parseFlingLogandExpr()
+{
+}
+void Parser::_parseFlingCmpEqEtcExpr()
+{
+}
+void Parser::_parseFlingCmpLtEtcExpr()
+{
+}
+void Parser::_parseFlingPlusMinusExpr()
+{
+}
+void Parser::_parseFlingMulDivModExpr()
+{
+}
+void Parser::_parseFlingBitorBitnorExpr()
+{
+}
+void Parser::_parseFlingBitandBitnandExpr()
+{
+}
+void Parser::_parseFlingBitxorBitxnorExpr()
+{
+}
+void Parser::_parseFlingBitshiftExpr()
+{
+}
+void Parser::_parseFlingUnaryExpr()
+{
+}
+void Parser::_parseFlingLowExpr()
+{
+}
+void Parser::_parseFlingCallDollarFuncExpr()
+{
+}
+void Parser::_parseFlingCallSubprogExpr()
+{
+}
+void Parser::_parseFlingSubprogIdent()
+{
+}
+void Parser::_parseFlingIdentExpr()
+{
+}
+void Parser::_parseFlingCatExpr()
+{
+}
+void Parser::_parseFlingReplExpr()
+{
+}
+void Parser::_parseFlingRange()
+{
+}
+void Parser::_parseFlingNonSimpleRange()
+{
+}
+void Parser::_parseFlingSimpleRangeSuffix()
+{
+}
+void Parser::_parseFlingExprOrRange()
+{
+}
+void Parser::_parseFlingTypenmOrModnm()
+{
+}
+
+} // namespace fling_hdl
--- src/parser_class.hpp
+++ src/parser_class.hpp
@@ -9,8 +9,138 @@
 namespace fling_hdl
 {
 
-class Parser
+class Parser: public RdParserBase<Lexer, Parser>
 {
+public:		// types
+	using Base = RdParserBase<Lexer, Parser>;
+
+public:		// functions
+	inline Parser(const string& s_filename)
+		: Base(s_filename)
+	{
+	}
+	GEN_NO_CM_CONSTRUCTORS_AND_ASSIGN(Parser);
+	virtual inline ~Parser() = default;
+
+	void parseFlingProgram();
+
+private:		// functions
+	void _parseFlingDeclPackage();
+	void _parseFlingDeclPackageItem();
+	void _parseFlingImport();
+	void _parseFlingDeclParamList();
+	void _parseFlingDeclParamListItem();
+	void _parseFlingDeclArgList();
+	void _parseFlingDeclArgListItem();
+	void _parseFlingInstParamList();
+	void _parseFlingInstParamListPos();
+	void _parseFlingInstParamListPosItem();
+	void _parseFlingInstParamListNamed();
+	void _parseFlingInstParamListNamedItem();
+	void _parseFlingInstArgList();
+	void _parseFlingInstArgListPos();
+	void _parseFlingInstArgListPosItem();
+	void _parseFlingInstArgListNamed();
+	void _parseFlingInstArgListNamedItem();
+	void _parseFlingDeclModule();
+	void _parseFlingDeclModuleScope();
+	void _parseFlingDeclModuleItem();
+	void _parseFlingModinst();
+	void _parseFlingDeclModuleGen();
+	void _parseFlingDeclModuleGenIf();
+	void _parseFlingDeclModuleGenSwitchEtc();
+	void _parseFlingDeclModuleGenSwitchEtcCase();
+	void _parseFlingDeclModuleGenSwitchEtcDefault();
+	void _parseFlingDeclModuleGenFor();
+	void _parseFlingDeclModuleBehav();
+	void _parseFlingDeclModuleBehavComb();
+	void _parseFlingDeclModuleBehavSeq();
+	void _parseFlingDeclModuleBehavSeqEdgeItem();
+	void _parseFlingDeclModuleBehavScope();
+	void _parseFlingDeclModuleBehavScopeItem();
+	void _parseFlingDeclModuleBehavScopeItemIf();
+	void _parseFlingDeclModuleBehavScopeItemSwitchEtc();
+	void _parseFlingDeclModuleBehavScopeItemSwitchEtcCase();
+	void _parseFlingDeclModuleBehavScopeItemSwitchEtcDefault();
+	void _parseFlingDeclModuleBehavScopeItemFor();
+	void _parseFlingDeclModuleBehavScopeItemWhile();
+	void _parseFlingDeclModuleBehavScopeItemGen();
+	void _parseFlingDeclModuleBehavScopeItemGenIf();
+	void _parseFlingDeclModuleBehavScopeItemGenSwitchEtc();
+	void _parseFlingDeclModuleBehavScopeItemGenSwitchEtcCase();
+	void _parseFlingDeclModuleBehavScopeItemGenSwitchEtcDefault();
+	void _parseFlingDeclModuleBehavScopeItemGenFor();
+	void _parseFlingAnyBehavScopeItem();
+	void _parseFlingAnyBehavScopeItemAssign();
+	void _parseFlingDeclCompositeType();
+	void _parseFlingDeclCompositeTypeStruct();
+	void _parseFlingDeclCompositeTypeStructItem();
+	void _parseFlingCompositeTypeStructItemGen();
+	void _parseFlingCompositeTypeStructItemGenIf();
+	void _parseFlingCompositeTypeStructItemGenSwitchEtc();
+	void _parseFlingCompositeTypeStructItemGenSwitchEtcCase();
+	void _parseFlingCompositeTypeStructItemGenSwitchEtcDefault();
+	void _parseFlingCompositeTypeStructItemGenFor();
+	void _parseFlingDeclEnum();
+	void _parseFlingDeclEnumItem();
+	void _parseFlingDeclSubprog();
+	void _parseFlingDeclSubprogFuncHeader();
+	void _parseFlingDeclSubprogTaskHeader();
+	void _parseFlingDeclSubprogScope();
+	void _parseFlingDeclSubprogScopeItem();
+	void _parseFlingDeclSubprogScopeItemIf();
+	void _parseFlingDeclSubprogScopeItemSwitchEtc();
+	void _parseFlingDeclSubprogScopeItemSwitchEtcCase();
+	void _parseFlingDeclSubprogScopeItemSwitchEtcDefault();
+	void _parseFlingDeclSubprogScopeItemFor();
+	void _parseFlingDeclSubprogScopeItemWhile();
+	void _parseFlingDeclSubprogScopeItemGen();
+	void _parseFlingDeclSubprogScopeItemGenIf();
+	void _parseFlingDeclSubprogScopeItemGenSwitchEtc();
+	void _parseFlingDeclSubprogScopeItemGenSwitchEtcCase();
+	void _parseFlingDeclSubprogScopeItemGenSwitchEtcDefault();
+	void _parseFlingDeclSubprogScopeItemGenFor();
+	void _parseFlingDeclConst();
+	void _parseFlingDeclVarNoInitVal();
+	void _parseFlingDeclVar();
+	void _parseFlingDeclWire();
+	void _parseFlingWireAssign();
+	void _parseFlingDeclAlias();
+	void _parseFlingIdent();
+	void _parseFlingScopedIdent();
+	void _parseFlingIdentList();
+	void _parseFlingSubprogIdentList();
+	void _parseFlingExprList();
+	void _parseFlingRangeList();
+	void _parseFlingExprOrRangeList();
+	void _parseFlingTypenmOrModnmList();
+	void _parseFlingImportItem();
+	void _parseFlingImportItemList();
+	void _parseFlingExpr();
+	void _parseFlingMuxExpr();
+	void _parseFlingLogorExpr();
+	void _parseFlingLogandExpr();
+	void _parseFlingCmpEqEtcExpr();
+	void _parseFlingCmpLtEtcExpr();
+	void _parseFlingPlusMinusExpr();
+	void _parseFlingMulDivModExpr();
+	void _parseFlingBitorBitnorExpr();
+	void _parseFlingBitandBitnandExpr();
+	void _parseFlingBitxorBitxnorExpr();
+	void _parseFlingBitshiftExpr();
+	void _parseFlingUnaryExpr();
+	void _parseFlingLowExpr();
+	void _parseFlingCallDollarFuncExpr();
+	void _parseFlingCallSubprogExpr();
+	void _parseFlingSubprogIdent();
+	void _parseFlingIdentExpr();
+	void _parseFlingCatExpr();
+	void _parseFlingReplExpr();
+	void _parseFlingRange();
+	void _parseFlingNonSimpleRange();
+	void _parseFlingSimpleRangeSuffix();
+	void _parseFlingExprOrRange();
+	void _parseFlingTypenmOrModnm();
 };
 
 } // namespace fling_hdl
